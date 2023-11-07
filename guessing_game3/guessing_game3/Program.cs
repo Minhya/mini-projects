cd6db0b671c26e8079d83ba05d0d77c4987677fb
Minhya <minhy96th@gmail.com>
2023-11-07 20:46:38 +0100

    finished product

--- guessing_game3/guessing_game3/Program.cs
+++ guessing_game3/guessing_game3/Program.cs
@@ -9,40 +9,36 @@ namespace annagissningsspel2
         {
 
             Program.GuessingGame(args);
-
-
-
         }
         static void GuessingGame(string[] args)
         {
-            Random rnd = new Random();
-            int rngnum = rnd.Next(1, 101); // Fixed the range to 1-100
 
-            Console.WriteLine("Welcome to Anna's guessing game!");
-
-            while (true)
+            while (true) // while loop för att loopa hela spelet
             {
+                Random rnd = new Random(); // generar ett slumpmässigt nummer
+                int rngnum = rnd.Next(1, 101); // lägger en gräns så att det är mellan 1 - 100
 
+                Console.WriteLine("Welcome to Anna's guessing game!");
 
-                Console.WriteLine("Are you ready to play? Type 'yes' to continue....");//new change cuaz im cool
+                Console.WriteLine("Are you ready to play? Type 'yes' to continue....");
                 string answer_to_play = Console.ReadLine().ToLower();
 
-                int maxAttempts = 3;
+                int maxAttempts = 3; 
 
                 if (answer_to_play.ToLower() == "yes")
                 {
                     int attempts = 0;
 
-                    while (attempts < maxAttempts)
+                    while (attempts < maxAttempts) 
                     {
                         Console.WriteLine("Guess a number from 1 to 100: ");
                         int userGuess;
 
-                        if (int.TryParse(Console.ReadLine(), out userGuess))
+                        if (int.TryParse(Console.ReadLine(), out userGuess))  // TryParse() converts the string data type into int Boolean if its is within the limit. out stores the userguesses
                         {
                             if (userGuess < 1 || userGuess > 100)
                             {
-                                Console.WriteLine("Enter a number between 1 and 100.");
+                                attempts++;
                             }
                             else
                             {
@@ -74,20 +70,17 @@ namespace annagissningsspel2
                     {
                         Console.WriteLine("Game over. You lost. The correct number was " + rngnum + ". Type 'yes' to try again.");
                         answer = Console.ReadLine().ToLower();
-                        if (answer == "no")
+                        if (answer != "yes")
                         {
                             Console.WriteLine("All done!");
                             break;
                         }
                     }
-                    Console.WriteLine("Do you want to continue (yes/no)?");
-                    answer = Console.ReadLine().ToLower();
-                    if (answer == "no")
-                    {
-                        Console.WriteLine("All done!");
-                        break;
-                    }
+                    
                 }
+                 else Console.WriteLine("Bye!");
+                 answer_to_play = Console.ReadLine().ToLower();
+                 break;
             }
         }
 
