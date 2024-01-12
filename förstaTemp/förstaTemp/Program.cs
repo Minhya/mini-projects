7a521b2034ab617bae1ab799fcc010e58cce9e04
Minhya <minhy96th@gmail.com>
2024-01-12 20:13:23 +0100

    ff

--- förstaTemp/förstaTemp/Program.cs
+++ förstaTemp/förstaTemp/Program.cs
@@ -2,14 +2,14 @@
 
 class Program
 {
-    static int FahrToCels(int fahrenheit)
+    static int FahrToCels(int fahrenheit) //metod för att konvertera fahrenheit till celsius.
     {
-        int celcius = (int)((fahrenheit - 32) * 5.0 / 9.0);
+        int celcius = (int)((fahrenheit - 32) * 5.0 / 9.0); //uträkningen för celsius.
         return celcius;
 
     }
 
-    public static void Main(string[] args)
+    public static void Main(string[] args)//metoden för att mata in fahrenheit samt att den kommer att använda sig av FahrToCels metoden.
     {
 
         Console.WriteLine("Skriv temperaturen i fahrenheit: ");
